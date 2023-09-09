a8d8fac2fff81222cd65d4427c264a7d4e22d44e
Abdelrahman_Abass <92057185+Abdelrhman-Abass@users.noreply.github.com>
2023-09-09 21:49:06 +0300

    gpt3-app

---
+++ src/components/cta/CTA.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./cta.css"
+const CTA = () => {
+  return (
+    <div className='gpt3__cta section__padding'>
+      <div className='gpt3__cta-content'>
+        <h3>Request Early Access to Get Started</h3>
+        <p>Register today & start exploring the endless possiblities.</p>
+      </div>
+      <div className='gpt3__cta-button'>
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
+  )
+}
+
+export default CTA
